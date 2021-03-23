777ed15985bd4368a805262dbaf786b7b391e358
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-23 16:07:18 +0100

    Estrapolazione categorie

--- js/script.js
+++ js/script.js
@@ -126,20 +126,37 @@ const icons = [
 
 // Milestone 1
 // Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
-const containerHTML = $('.icons');
+const iconsContainer = $('.icons');
+const colors = ['pink', 'lightblue', 'brown'];
 
-icons.forEach( (icon) => {
-  const {name, family, prefix, category} = icon;
-  containerHTML.append(`
-    <div>
-      <i class="${family} ${prefix}${name}"></i>
+function generateIcons (array, html, color) {
+  array.forEach( (icon) => {
+    const {name, family, prefix, category} = icon;
+    html.append(`
+      <div>
+      <i style="color:${color}" class="${family} ${prefix}${name}"></i>
       <div class="title">${name}</div>
-    </div>
-  `);
-});
+      </div>
+    `);
+  });
+}
 
 
 // Milestone 2
 // Coloriamo le icone per categoria
+const iconCategories = [];
+
+icons.forEach( (icon) => {
+  if ( iconCategories.length == 0 ) {
+    iconCategories.push(icon.category);
+  } else if ( iconCategories.includes(icon.category) == false ) {
+    iconCategories.push(icon.category);
+  }
+});
+
+console.log(iconCategories);
+
+generateIcons(icons, iconsContainer, iconColor);
+
 // Milestone 3
 // Creiamo una select con le categorie di icone e usiamola per filtrare le icone
